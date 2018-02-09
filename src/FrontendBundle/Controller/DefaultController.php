173d647a2ba3a5ae74d369857c9db964570c20c1
wolverine20 <arandapablodamian@gmail.com>
2018-02-09 05:36:38 +0100

    comienzo del frontend

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -12,6 +12,38 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('FrontendBundle::template.html.twig');
+        return $this->render('FrontendBundle::index.html.twig');
     }
+
+   	 /**
+     * @Route("/contacto")
+     */
+    public function contactoAction()
+    {
+        return $this->render('FrontendBundle::contacto.html.twig');
+    }
+     /**
+     * @Route("/nosotros")
+     */
+    public function nostrosAction()
+    {
+        return $this->render('FrontendBundle::nosotros.html.twig');
+    }
+     /**
+     * @Route("/galeria")
+     */
+    public function galeriaAction()
+    {
+        return $this->render('FrontendBundle::galeria.html.twig');
+    }
+
+     /**
+     * @Route("/tienda")
+     */
+    public function tiendaAction()
+    {
+        return $this->render('FrontendBundle::tienda.html.twig');
+    }
+
+
 }
